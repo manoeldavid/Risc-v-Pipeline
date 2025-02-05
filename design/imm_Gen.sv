759f04f0ff7b364d090308df325e6704ae5869ed
praxarr <151402347+praxarr@users.noreply.github.com>
2025-02-05 18:01:06 -0300

    Update imm_Gen.sv
    adicionei as modificacoes para implementar o imediato de jal e jalr, e fiz algumas correcoes no codigo

--- design/imm_Gen.sv
+++ design/imm_Gen.sv
@@ -12,14 +12,27 @@ module imm_Gen (
       Imm_out = {inst_code[31] ? 20'hFFFFF : 20'b0, inst_code[31:20]};
 
       7'b0010011: //*I-type (addi, slti, andi, etc
-      if((inst_code[14:12] == 3'b001) || inst_code[14:12] == 3'b101) {
-	  Imm_out = {27'b, inst_code[24-20]}; // se for srai, srli, slli
-      else
-      	Imm_out = {inst_code[31] ? 20'hFFFFF : 20'b0, inst_code[31:20]};
-
+      begin
+      	if((inst_code[14:12] == 3'b001) || inst_code[14:12] == 3'b101) {
+	  	Imm_out = {7'b0, inst_code[24-20]}; // se for srai, srli, slli
+      	else
+      		Imm_out = {inst_code[31] ? 20'hFFFFF : 20'b0, inst_code[31:20]};
+      end
+      7'b1101111: // JAL
+      Imm_out = {
+      	inst_code[31] ? 11'h7FF : 11'b0,
+        inst_code[31],
+        inst_code[19:12],
+        inst_code[20],
+        inst_code[30:21],
+        1'b0
+       };
+      7'b1100111: // JALR
+      Imm_out = {inst_code[31] ? 20'hFFFFF : 20'b0, inst_code[31:20]};
       7'b0100011:  /*S-type*/
       Imm_out = {inst_code[31] ? 20'hFFFFF : 20'b0, inst_code[31:25], inst_code[11:7]}; // verifica se o numero eh negativo. se for positivo, os 20 primeiros valores sao 0
-
+	
+	
       7'b1100011:  /*B-type*/
       Imm_out = {
         inst_code[31] ? 19'h7FFFF : 19'b0,  // verifica se o numero eh negativo
