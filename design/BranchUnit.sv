84f2bd7c932aaaab0d1655aace56bafc1d8ccb96
praxarr <151402347+praxarr@users.noreply.github.com>
2025-02-05 16:15:46 -0300

    Update BranchUnit.sv

--- design/BranchUnit.sv
+++ design/BranchUnit.sv
@@ -14,6 +14,7 @@ module BranchUnit #(
 );
 
   logic Branch_Sel;
+  logic Jalr_Sel;
   logic [31:0] PC_Full;
 
   assign PC_Full = {23'b0, Cur_PC};
@@ -21,8 +22,10 @@ module BranchUnit #(
   assign PC_Imm = PC_Full + Imm;
   assign PC_Four = PC_Full + 32'b100;
   assign Branch_Sel = Branch && AluResult[0];  // 0:Branch is taken; 1:Branch is not taken
+ 
+    assign Jalr = (AluResult != 0 && AluResult != 1) ? 1 : 0: // verifica se a instrucao em questao eh JAL_R
 
-  assign BrPC = (Branch_Sel) ? PC_Imm : 32'b0;  // Branch -> PC+Imm   // Otherwise, BrPC value is not important
-  assign PcSel = Branch_Sel;  // 1:branch is taken; 0:branch is not taken(choose pc+4)
+  assign BrPC = (Jalr) ? AluResult : (Branch_Sel) ? Pc_Imm : 32'b0;  // Branch -> PC+Imm   // se for JALR, carrega o endereco a partir da ALU. Se for Branch, carrega a partir de PC + IMM. se nao for branch, BR_PC eh ignorado
+  assign PcSel = Branch_Sel || Jalr;  // 1:branch is taken; 0:branch is not taken(choose pc+4)
 
 endmodule
