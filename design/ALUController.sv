e6db24602555ae65d6228e1d9d51427bb4c3e36c
praxarr <151402347+praxarr@users.noreply.github.com>
2025-02-05 15:42:00 -0300

    Update
    coloquei o JAL e o JALR

--- design/ALUController.sv
+++ design/ALUController.sv
@@ -20,12 +20,13 @@ module ALUController (
       ((ALUOp == 2'b10) && (Funct3 == 3'b100))  // R\I XOR
       ((ALUOp == 2'b10) && (Funct3 == 3'b010)); // SLTI
 
-  assign Operation[1] = (ALUOp == 2'b00) ||  // LW\SW
+  assign Operation[1] = (ALUOp == 2'b00) ||  // LW\SW\JAlR
       ((ALUOp == 2'b10) && (Funct3 == 3'b000)) ||  // R\I-add
       ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000));  // R\I->>>
       ((ALUOp == 2'b01) && (Funct3 == 3'b100)) || // BLT
       ((ALUOp == 2'b01) && (Funct3 == 3'b101))  // BGE
       ((ALUOp == 2'b10) && (Funct3 == 3'b010)); // SLTI
+      ((ALUOp == 2'b11)); // JAL
        
 
   assign Operation[2] =  ((ALUOp==2'b10) && (Funct3==3'b101) && (Funct7==7'b0000000)) || // R\I->>
@@ -34,14 +35,16 @@ module ALUController (
       ((ALUOp == 2'b10) && (Funct3 == 3'b010))  // R\I-<
       ((ALUOp == 2'b01) && (Funct3 == 3'b101))  // BGE
       ((ALUOp == 2'b10) && (Funct3 == 3'b110));  // R\I OR
+      ((ALUOp == 2'b11));  // JAL
 
 
-  assign Operation[3] = ((ALUOp == 2'b01) ||  // BEQ
+  assign Operation[3] = ((ALUOp == 2'b01) ||// BEQ
       ((ALUOp == 2'b10) && (Funct3 == 3'b010)) ||  // R\I-<
       ((ALUOp == 2'b01) && (Funct3 == 3'b001)) || // BNEQ
       ((ALUOp == 2'b01) && (Funct3 == 3'b100)) || // BLT
       ((ALUOp == 2'b01) && (Funct3 == 3'b101))  // BGE
       ((ALUOp == 2'b10) && (Funct3 == 3'b110))  // R\I OR
-      ((ALUOp == 2'b10) && (Funct3 == 3'b010)); // SLTI
+      ((ALUOp == 2'b10) && (Funct3 == 3'b010)) // SLTI
+      ((ALUOp == 2'b11));  // JAL
 
 endmodule
