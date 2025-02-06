6cd2511c86cb078231db602ec09e125832da3bfa
praxarr <151402347+praxarr@users.noreply.github.com>
2025-02-06 19:56:47 -0300

    Update ALUController.sv

--- design/ALUController.sv
+++ design/ALUController.sv
@@ -15,7 +15,7 @@ module ALUController (
       ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0000000)) ||  // R\I->>
       ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000)) ||  // R\I->>>
       ((ALUOp == 2'b01) && (Funct3 == 3'b001)) || // BNEQ
-      ((ALUOp == 2'b01) && (Funct3 == 3'b100)) || // BLT
+      ((ALUOp == 2'b01) && (Funct3 == 3'b100)) ||/// BLT
       ((ALUOp == 2'b01) && (Funct3 == 3'b101)) || // BGE
       ((ALUOp == 2'b10) && (Funct3 == 3'b100)) ||// R\I XOR
       ((ALUOp == 2'b10) && (Funct3 == 3'b010)); // SLTI
@@ -23,8 +23,8 @@ module ALUController (
   assign Operation[1] = (ALUOp == 2'b00) ||  // LW\SW\JAlR
       ((ALUOp == 2'b10) && (Funct3 == 3'b000)) ||  // R\I-add
       ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000)) || // R\I->>>
-      ((ALUOp == 2'b01) && (Funct3 == 3'b100)) || // BLT
       ((ALUOp == 2'b01) && (Funct3 == 3'b101)) || // BGE
+      ((ALUOp == 2'b01) && (Funct3 == 3'b100)) ||// BLT
       ((ALUOp == 2'b10) && (Funct3 == 3'b010)) ||// SLTI
       ((ALUOp == 2'b10) && (Funct3 == 3'b010)) ||// R\I-<
       ((ALUOp == 2'b11)); // JAL
@@ -34,15 +34,16 @@ module ALUController (
       ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000)) ||  // R\I->>>
       ((ALUOp == 2'b10) && (Funct3 == 3'b001)) ||  // R\I-<<
       ((ALUOp == 2'b01) && (Funct3 == 3'b101)) ||// BGE
+      ((ALUOp == 2'b10) && (Funct3 == 3'b100)) ||// R\I XOR
       ((ALUOp == 2'b11));  // JAL
 
 
-  assign Operation[3] = (ALUOp == 2'b01) ||// BEQ
+  assign Operation[3] = ((ALUOp == 2'b01) && (Funct3 == 3'b000)) ||// BEQ
       ((ALUOp == 2'b10) && (Funct3 == 3'b010)) ||  // R\I-<
       ((ALUOp == 2'b01) && (Funct3 == 3'b001)) || // BNEQ
-      ((ALUOp == 2'b01) && (Funct3 == 3'b100)) || // BLT
+      ((ALUOp == 2'b01) && (Funct3 == 3'b100)) ||                          // BLT
       ((ALUOp == 2'b01) && (Funct3 == 3'b101)) || // BGE
-      ((ALUOp == 2'b10) && (Funct3 == 3'b110)) || // R\I OR
+      ((ALUOp == 2'b10) && (Funct3 == 3'b100)) ||// R\I XOR
       ((ALUOp == 2'b10) && (Funct3 == 3'b010)) ||// SLTI
       ((ALUOp == 2'b11));  // JAL
 
