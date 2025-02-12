02ec6f35a00a06a71eaa3fec01026d396b289158
praxarr <151402347+praxarr@users.noreply.github.com>
2025-02-11 21:30:57 -0300

    Update BranchUnit.sv

--- design/BranchUnit.sv
+++ design/BranchUnit.sv
@@ -7,6 +7,7 @@ module BranchUnit #(
     input logic [31:0] Imm,
     input logic Branch,
     input logic [31:0] AluResult,
+    input logic Halt_Insert,
     output logic [31:0] PC_Imm,
     output logic [31:0] PC_Four,
     output logic [31:0] BrPC,
@@ -20,12 +21,12 @@ module BranchUnit #(
   assign PC_Full = {23'b0, Cur_PC};
 
   assign PC_Imm = PC_Full + Imm;
-  assign PC_Four = PC_Full + 32'b100;
+  assign PC_Four = (Halt_Insert) ? PC_Full : PC_Full + 32'b100;
   assign Branch_Sel = Branch && AluResult[0];  // 0:Branch is taken; 1:Branch is not taken
  
   assign Jalr_Sel = (AluResult != 0 && AluResult != 1 && Branch == 1) ? 1 : 0;
 
-  assign BrPC = (Jalr_Sel) ? AluResult : (Branch_Sel) ? PC_Imm : 32'b0;  // Branch -> PC+Imm   // Otherwise, BrPC value is not importat
-  assign PcSel = Branch_Sel || Jalr_Sel;  // 1:branch is taken; 0:branch is not taken(choose pc+4)
+  assign BrPC = (Halt_Insert) ? PC_Full : (Jalr_Sel) ? AluResult : (Branch_Sel) ? PC_Imm : 32'b0;  // Branch -> PC+Imm   // Otherwise, BrPC value is not importat
+  assign PcSel = (Branch_Sel) || (Jalr_Sel) || (Halt_Insert);  // 1:branch is taken; 0:branch is not taken(choose pc+4)
 
 endmodule
